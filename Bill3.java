976ddc0ff8ff4b3928b04154dd2e55a7ac0eb4d8
Yosef <yosef.salamaruah@post.runi.ac.il>
2024-11-14 18:29:20 +0200

    upload

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,8 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		double money = Double.parseDouble(args[3]);
-		money = money/3;
+		money = money/3.0;
+		money = Math.ceil (money);
 		System.out.println("Dear "+ name3+", "+name2+" and "+name1+": pay " + money +" Shekels each.");
 	
 	}
